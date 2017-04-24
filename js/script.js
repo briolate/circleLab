ebf3b4eb52c375609895a9a5fdb5fe8c0484b58f
Evan Briolat <briolate@gmail.com>
2017-04-24 09:01:03 -0400

    First commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function calcArea(){
+  var circle = document.createElement('div');
+  var radius = document.getElementById('radius').value;
+  var area = Math.PI * (radius * 2);
+  var totalArea = document.createElement('h2');
+  totalArea.innerText = 'The area of your moon is ' + area.toFixed(2);
+  circle.className = "circle";
+  circle.style.width = radius * 2 +'px';
+  circle.style.height = radius * 2 +'px';
+  document.body.appendChild(totalArea);
+  document.body.appendChild(circle);
+}
+
+function reload() {
+    location.reload();
+}
